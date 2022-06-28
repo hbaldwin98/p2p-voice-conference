b0d10c08335937456d4d978b180df2747d71c0d9
hbaldwin98 <huntercbaldwin98@gmail.com>
2022-06-27 20:31:45 -0700

    removed turn servers for testing purpose

--- src/app/shared/services/webrtc.service.ts
+++ src/app/shared/services/webrtc.service.ts
@@ -28,13 +28,7 @@ export class WebRTCService {
   configuration = {
     iceServers: [ {
       urls: 'stun:stun.l.google.com:13902',
-    }, {
-      urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject",
-    }, {
-      urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject",
-    }, {
-      urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject",
-    } ]
+    }]
   };
 
   constructor(private channel: ChannelService, public socket: Socket) {}
